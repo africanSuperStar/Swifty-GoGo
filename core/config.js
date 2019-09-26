46e1dbaab7ef9054766b26bd6f5b98372e4d192c
Sarel de Beer <boertjie.seun@outlook.com>
2019-09-26 07:57:09 +0200

    Removing this file

--- core/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: "AIzaSyCjHQ-qASlJgo5Ri2PMyUwB8xBla3rrOXk",
-    appId: "1:699640229265:web:d347a9658d4d16ee453e30",
-    senderId: '699640229265',
-};
-
-export {
-    config
-};
